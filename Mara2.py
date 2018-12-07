172f1761d7bf804ea1dfde331c10ae6186a6b446
MUGUNGA Hervé <henzy80@hotmail.com>
2018-12-07 17:27:35 +0200

    multiplication table

---
+++ Mara2.py
@@ -0,0 +1,12 @@
+
+        n=int(input("Enter a number of multiplication tables you want to display : "))
+        m=int(input("Enter the last number to be multiplied: "))
+        for i in range(2,n+1):
+                print("\n"" Table number ",i,"\n")
+        for r in range (1,m+1):
+      
+                print(i,"x",r,"=",i*r)
+
+print(" press Enter to exit ")
+ 
+    
